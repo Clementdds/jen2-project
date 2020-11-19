f872d5f303c3982864d99773e0b4b61ec570ca68
Thomas <thomas.curti@epita.fr>
2020-11-19 14:55:11 +0100

    Parser: Add parser

---
+++ PassTheTest/src/main/java/fr/epita/tfidf/DocParser.java
@@ -0,0 +1,50 @@
+package fr.epita.tfidf;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class DocParser {
+
+    private Document doc = null;
+
+    public DocParser(String url) {
+        try {
+            this.doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public DocParser(Document document) {
+        this.doc = document;
+    }
+
+    /*
+     * Setters
+     */
+    public void setDoc(String url){
+        try {
+            this.doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setDoc(Document document) {
+        this.doc = document;
+    }
+
+    /*
+     * Getters
+     */
+    public Document getDoc(){
+        return this.doc;
+    }
+
+    public String getText(){
+        return this.doc.body().text();
+    }
+
+}
---
+++ PassTheTest/src/test/java/fr/epita/hivers/DocParserTest.java
@@ -0,0 +1,38 @@
+package fr.epita.hivers;
+
+import fr.epita.tfidf.DocParser;
+import org.jsoup.Jsoup;
+import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
+
+import org.jsoup.nodes.Document;
+import javax.xml.parsers.ParserConfigurationException;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DocParserTest {
+
+    @Test
+    public void ParseHtmlFromUrl() {
+        DocParser docParser = new DocParser("https://example.com/");
+        String text = docParser.getText();
+
+        assertEquals(
+                "Example Domain This domain is for use in illustrative examples in documents. You may use this domain" +
+                        " in literature without prior coordination or asking for permission. More information...",
+                text);
+    }
+
+    @Test
+    public void ParseHtmlFromDocument() {
+        String html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
+        Document document = Jsoup.parse(html);
+
+        DocParser docParser = new DocParser(document);
+        String text = docParser.getText();
+
+        assertEquals("An example link.", text);
+    }
+}
